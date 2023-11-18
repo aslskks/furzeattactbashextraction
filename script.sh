b776c69d32f0d47751286561d157414633e84dec
aslskks <130728002+aslskks@users.noreply.github.com>
2023-11-18 17:03:26 -0600

    code

--- script.sh
+++ script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-imagen = "$1"
+imagen="$1"
 diccionario="$2"
 
 while IFS= read -r password; do
-  echo "Probando contrasena: $password" 
+  echo "Probando contraseña: $password"
   steghide extract -sf "$imagen" -p "$password" &>/dev/null
-  if [ $? -eq 0]; then
-    echo "Extraccion exitosa con contraseña: $password¨
+  if [ $? -eq 0 ]; then
+    echo "Extracción exitosa con contraseña: $password"
     exit 0
   fi
 done < "$diccionario"
 
-echo "No se pudo extraer el arachivo oculto. Se probaron todas las contraseñas."
+echo "No se pudo extraer el archivo oculto. Se probaron todas las contraseñas."
 exit 1
